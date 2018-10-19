20b48420e23b17d61e2852a3d6dfbf58e631d9ad
Tanner Strickler <tstrickler@ku.edu>
2018-10-19 10:31:06 -0500

    update page title and appearance

--- ViewUsers.php
+++ ViewUsers.php
@@ -1,4 +1,10 @@
 <?php
+    echo "<html>";
+    echo "<head>";
+    echo "<title>View Users</title>";
+    echo "</head>";
+    echo "<body>";
+    echo "<h1>View Users</h1>";
     $mysqli = new mysqli("mysql.eecs.ku.edu", "tstrickler", "vai4Chai", "tstrickler");
     if ($mysqli->connect_errno) {
         printf("Failed to connect: %s\n", $mysqli->connect_error);
@@ -15,4 +21,7 @@
     }
 
     $mysqli->close();
+
+    echo "</body>";
+    echo "</html>";
 ?>
